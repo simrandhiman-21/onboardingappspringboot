49afaf41d8ff321017386cdd2810deb8a9d62986
simrandhiman <simrandhiman3121@gmail.com>
2025-05-29 10:54:22 +0530

    [Simran added app]

---
+++ src/main/java/com/example/OnBoardingApp/OnBoardingAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.OnBoardingApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnBoardingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnBoardingAppApplication.class, args);
+	}
+
+}
---
+++ src/main/java/org/learning/controller/Home.java
@@ -0,0 +1,15 @@
+package org.learning.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class Home {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "WELCOME TO APP";
+    }
+}
